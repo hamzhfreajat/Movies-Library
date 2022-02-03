c5a77ee2b22c2bad95f24f0a94f9901e5f52c2bd
Hamzh Freijat <hamzhg2016@gmail.com>
2022-02-03 09:40:37 +0200

    solved

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const data = require('./data/data.json');
 const express = require('express');
 const cors = require('cors');
 const axios = require("axios");
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 
+const PORT = process.env.PORT;
 let userSearch = "Spider-Man";
 const trendUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.APIKEY}&language=en-US`;
 let chargeUrl = `https://api.themoviedb.org/3/movie/changes?api_key=${process.env.APIKEY}&page=1`;
@@ -19,6 +23,12 @@ server.get('/trending', handleTrendingMovies);
 server.get('/search', handleSearchMovies);
 server.get('/changes' , handelChanges);
 server.get('/certificat' , handleCertifate)
+server.post('/addMovie',addMovies);
+server.put('/Update/:id',updateMovie);
+server.delete('/DELETE/:id',deleteMovie);
+server.get('/getOneMovies/:id',getOneMovies);
+server.get('/getMovies',getMovies);
+
 server.get('*',handelNotFound);
 server.use(errorHandler);
 
@@ -34,7 +44,61 @@ function handelFavorite(req,res){
     res.status(200).send("Welcome to Favorite Page")
 }
 
+function addMovies(req,res){
+  const movie = req.body;
+  console.log(req.body);
+  let sql = `INSERT INTO movies_data(title,movies_path,overview,comment) VALUES ($1,$2,$3,$4) RETURNING *;`
+  let values=[movie.title,movie.movies_path,movie.overview,movie.comment];
+  client.query(sql,values).then(data =>{
+      res.status(200).json(data.rows);
+  }).catch(error=>{
+      errorHandler(error,req,res)
+  });
+}
+function getMovies(req,res){
+    let sql = `SELECT * FROM movies;`;
+    console.log("hi"); 
+    client.query(sql).then(data=>{
+        console.log(data); 
+       res.status(200).json(data.rows);
+    }).catch(error=>{
+        errorHandler(error,req,res)
+    });
+}
 
+function updateMovie(req,res){
+    const movie = req.body; 
+    const id = req.params.id; 
+    console.log('out'); 
+    const sql = `UPDATE movies_data SET title =$1, movies_path = $2, overview = $3 ,comment=$4  WHERE id=$5 RETURNING *;`; 
+    let values=[movie.title,movie.movies_path,movie.overview,movie.comment,id];
+    client.query(sql,values).then(data=>{
+        console.log('in')
+        res.status(200).json(data.rows);
+    }).catch(error=>{
+        errorHandler(error,req,res)
+    });
+}
+function deleteMovie(req,res){
+    const id = req.params.id;  
+    const sql = `DELETE FROM movies_data WHERE id=$1;`; 
+    let value= [id];
+    client.query(sql,value).then(data=>{
+        res.status(200).json(data.rows);
+    }).catch(error=>{
+        errorHandler(error,req,res)
+    });
+}
+function getOneMovies(req , res){
+    console.log('out'); 
+    const id = req.params.id;
+    const sql = `SELECT * FROM movies_data WHERE id=$1;`
+    const value = [id]; 
+    client.query(sql,value).then(data => {
+        console.log(data); 
+        res.status(200).json(data.rows); 
+    })
+}
 
 function handelData(req,res){
     let obj = new Movie(data.title, data.poster_path, data.overview);
@@ -47,7 +111,6 @@ function handelNotFound(req,res){
 }
 
 function handleTrendingMovies(req , res){
-
     axios.get(trendUrl).then((result)=>{
             let trendingMovies = result.data.results.map(item => {
                 return {
@@ -82,7 +145,7 @@ function handleSearchMovies  (req , res){
 function handelChanges(req,res) {
     axios.get(chargeUrl)
         .then((result)=>{
-            res.send(result.data);
+            res.json(result.data);
         }).catch((err)=>{
         errorHandler(err)
     })
@@ -91,22 +154,20 @@ function handelChanges(req,res) {
 function handleCertifate(req,res){
     axios.get(certificateUrl)
         .then((result)=>{
-            res.send(result.data);
+            res.json(result.data);
         }).catch((err)=>{
         errorHandler(err)
     })
 }
-function errorHandler (err, req, res, next) {
-    // res.status(500).json(
-    //     {
-    //         "status": 500,
-    //         "massage": err
-    //     }
-    // )
+function errorHandler (err, req, res) {
+    res.status(500).json(err); 
 }
-let port = 3000 ; 
-server.listen(port,()=>{
-    console.log(`you are in port: ${port}`);
+;
+// fourth is connecting the client
+client.connect().then(()=>{
+    server.listen(PORT,()=>{
+        console.log(`listining to port ${PORT}`)
+    })
 })
 
 
